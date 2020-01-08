d7832486aee429d67587f941c3b16b13acfc519c
Hanaa Sadoqi <hsadoqi@gmail.com>
2020-01-08 16:26:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
     var firstCustomer = katzDeliLine.shift();
-    return (`Currently serving ${firstCustomer}.`)
+    return `Currently serving ${firstCustomer}.`;
   } else {
-    return ('There is nobody waiting to be served!')
+    return 'There is nobody waiting to be served!';
 }
 }
 
@@ -18,11 +18,12 @@ function currentLine(katzDeliLine){
   var s = [];
   if (katzDeliLine.length > 0){
     for (let i = 0; i < katzDeliLine.length; i++){
-      s.push(` ${i + 1}` + '. ' + `${katzDeliLine[i]}`);
+      s.push(` ${i + 1}. ${katzDeliLine[i]}`);
     }
     return 'The line is currently:' + s
   } else {
-    return (`The line is currently empty.`)
+    return `The line is currently empty.`
   }
+  
 }
  
